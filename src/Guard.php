535b72bd4f476915934631fee83d703de4ff6aa0
Tomasz Pielczyl <tomasz.pielczyk@gmail.com>
2021-02-27 19:25:11 +0100

    add doc in code

--- src/Guard.php
+++ src/Guard.php
@@ -6,6 +6,12 @@ class Guard
 {
     protected $value;
 
+    /**
+     * Returns a Guard with the specified value
+     *
+     * @param mixed $value
+     * @return self
+     */
     public static function of($value)
     {
         return new static($value);
@@ -16,6 +22,14 @@ class Guard
         $this->value = $value;
     }
 
+    /**
+     * If called without argument, returns true if there is a value present, otherwise false.
+     * If called with argument and there is a value present, 
+     * invokes callback with value as first argument and returns self
+     *
+     * @param \Closure $callback
+     * @return boolean|self
+     */
     public function is($callback = null)
     {
         if (func_num_args() == 0) {
@@ -27,6 +41,13 @@ class Guard
         return $this;
     }
 
+    /**
+     * If called without argument, returns false if there is a value present, otherwise true.
+     * If called with argument and there is no value present, invokes callback and returns self
+     *
+     * @param \Closure $callback
+     * @return boolean|self
+     */
     public function not($callback = null)
     {
         if (func_num_args() == 0) {
@@ -38,16 +59,37 @@ class Guard
         return $this;
     }    
 
+    /**
+     * Return the value.
+     * If fallback is present and the value is null, than fallback is returned.
+     *
+     * @param \Closure $fallback
+     * @return mixed
+     */
     public function get($fallback = null)
     {
         return $this->is() ? $this->value : $fallback;
     }
 
+    /**
+     * Return the value.
+     * If fallback is present and the value is null, than fallback is invoked and result returned.
+     *
+     * @param \Closure $fallback
+     * @return mixed
+     */
     public function getFn($fallback)
     {
         return $this->is() ? $this->get() : $fallback();
     }
 
+    /**
+     * If a value is present and the value matches the given predicate,
+     * return a Guard describing the value, otherwise return an empty Guard
+     * 
+     * @param \Closure $predicate
+     * @return self
+     */
     public function filter($predicate)
     {
         if (!$this->is()) {
@@ -59,6 +101,13 @@ class Guard
         return static::of(null);
     }
 
+    /**
+     * If a value is present and the value matches the given predicate,
+     * return an empty Guard, otherwise return a Guard describing the value
+     * 
+     * @param \Closure $predicate
+     * @return self
+     */
     public function reject($predicate)
     {
         if (!$this->is()) {
@@ -70,6 +119,14 @@ class Guard
         return static::of(null);
     }
 
+    /**
+     * If a value is present, apply the provided mapping function to it,
+     * and if the result is non-null, return a Guard describing the result.
+     * Otherwise return an empty Guard.
+     *
+     * @param \Clouser $mapper
+     * @return self
+     */
     public function map($mapper)
     {
         if (!$this->is()) {
@@ -78,7 +135,15 @@ class Guard
         return static::of($mapper($this->get()));
     }
 
-    public function except($exception = null)
+    /**
+     * Throw exception if value is not present
+     * 
+     * If exception provided as class name, next arguments are passed to exception constructor
+     *
+     * @param string|object $exception Exception object or class name, default NoValueException
+     * @return self
+     */
+    public function except($exception = null, $msg = null)
     {
         if ($this->is()) {
             return $this;
@@ -93,6 +158,12 @@ class Guard
         throw $exception;
     }
 
+    /**
+     * If a current value is not present, return a Guard with value provided as argument
+     *
+     * @param mixed $value Value when current value is null
+     * @return self
+     */
     public function blank($value)
     {
         if ($this->is()) {
@@ -101,6 +172,12 @@ class Guard
         return static::of($value);
     }    
 
+    /**
+     * If a current value is not present, return a Guard with value provided by valueProvider
+     *
+     * @param \Closure $valueProvider Value provider when current value is null
+     * @return self
+     */
     public function blankFn($valueProvider)
     {
         if ($this->is()) {
@@ -108,6 +185,4 @@ class Guard
         }
         return static::of($valueProvider());
     }
-
-
 }
